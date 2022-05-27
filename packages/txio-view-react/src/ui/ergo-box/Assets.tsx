bf569ad8bd114211b485bf340aa0ca8e5d9ce2d7
ralf <ralf.weiss@email.de>
2022-05-27 07:02:16 +0200

    assets: name and type will only be displayed if there are any

--- packages/txio-view-react/src/ui/ergo-box/Assets.tsx
+++ packages/txio-view-react/src/ui/ergo-box/Assets.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ErgoBox } from "../../model";
+import * as R from "ramda";
 import { truncateWithEllipses } from "../../utils";
 
 interface AssetsProps {
@@ -15,32 +16,43 @@ interface AssetRowProps {
 
 const TRUNCATE_MAXLEN = 10;
 
-const assetRow = ({ name, type, tokenId, amount }: AssetRowProps) => (
-  <tr key={tokenId}>
-    <td>{name}</td>
-    <td>{type}</td>
-    <td>{truncateWithEllipses(TRUNCATE_MAXLEN)(tokenId)}</td>
-    <td style={{ width: "10ch", textAlign: "right" }}>{amount}</td>
-  </tr>
-);
+const assetRow =
+  ({ hasNames, hasTypes }) =>
+  ({ name, type, tokenId, amount }: AssetRowProps) =>
+    (
+      <tr key={tokenId}>
+        {hasNames ? <td>{name}</td> : null}
+        {hasTypes ? <td>{type}</td> : null}
+        <td>{truncateWithEllipses(TRUNCATE_MAXLEN)(tokenId)}</td>
+        <td style={{ width: "10ch", textAlign: "right" }}>{amount}</td>
+      </tr>
+    );
 
 export const Assets = ({ ergoBox }: AssetsProps) => {
   if (!ergoBox.assets || ergoBox.assets.length === 0) {
     return null;
   }
-  const Assets = ergoBox.assets.map(assetRow);
+  const hasNames =
+    R.filter(R.propEq("name", ""))(ergoBox.assets).length !==
+    ergoBox.assets.length;
+  const hasTypes =
+    R.filter(R.propEq("type", ""))(ergoBox.assets).length !==
+    ergoBox.assets.length;
+  const Assets = ergoBox.assets.map(assetRow({ hasNames, hasTypes }));
   return (
-    <table>
-      {/* <caption style={{textAlign:'left', fontSize: 'small'}}>assets</caption>         */}
-      <thead style={{ fontSize: "x-small" }}>
-        <tr style={{ width: "20ch" }}>
-          <th style={{ width: "10ch" }}>name</th>
-          <th style={{ width: "10ch" }}>type</th>
-          <th style={{ width: "10ch" }}>tokenId</th>
-          <th style={{ width: "10ch", textAlign: "right" }}>amount</th>
-        </tr>
-      </thead>
-      <tbody>{Assets}</tbody>
-    </table>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <table>
+        {/* <caption style={{textAlign:'left', fontSize: 'small'}}>assets</caption>         */}
+        <thead style={{ fontSize: "x-small" }}>
+          <tr style={{ width: "20ch" }}>
+            {hasNames ? <th style={{ width: "10ch" }}>name</th> : null}
+            {hasTypes ? <th style={{ width: "10ch" }}>type</th> : null}
+            <th style={{ width: "10ch" }}>tokenId</th>
+            <th style={{ width: "10ch", textAlign: "right" }}>amount</th>
+          </tr>
+        </thead>
+        <tbody>{Assets}</tbody>
+      </table>
+    </div>
   );
 };
